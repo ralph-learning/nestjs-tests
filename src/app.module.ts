9f3e2d8cb743434dbd7b3e4a177de70054c3a16e
Ralph Effting <ralfting@gmail.com>
2020-01-08 08:43:33 -0300

    Add mongodb configure it and create schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 
+
+
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ItemsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
