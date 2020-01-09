433f9ddcbd49fc130b6cb741ce8e3b60ef930875
Ralph Effting <ralfting@gmail.com>
2020-01-09 08:55:00 -0300

    Create controller actions (create, delete, update, findOne, findAll)

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
+import { ItemsService } from './items.service';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService){}
+
+  @Get()
+  async findAll(): Promise<Item[]> {
+    return await this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Item> {
+    console.log(id)
+    return await this.itemsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<Item> {
+    return this.itemsService.delete(id);
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id: string): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
+  }
+}
